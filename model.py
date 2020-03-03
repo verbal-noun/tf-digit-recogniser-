057a27cb87dfb240e00aae49be29a26bf43c12d0
verbal-noun <thats.kaif@gmail.com>
2020-03-03 22:26:43 +1100

    Setting up constants and hyperparameters

--- model.py
+++ model.py
@@ -6,3 +6,13 @@ from sklearn.model_selection import ShuffleSplit
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScalermnist = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
 
+LABELS = 10           # Number of labls(1-10)
+IMAGE_WIDTH = 28      # Width/height if the image
+COLOR_CHANNELS = 1    # Number of color channelsVALID_SIZE = 1000     # Size of the Validation dataEPOCHS = 20000        # Number of epochs to run
+BATCH_SIZE = 32       # SGD Batch size
+FILTER_SIZE = 5       # Filter size for kernel
+DEPTH = 32            # Number of filters/templates
+FC_NEURONS = 1024     # Number of neurons in the fully
+                      # connected later
+LR = 0.001            # Learning rate Alpha for SGD
+
