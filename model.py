0d91b5cc3ab81c7f4c09851ffd24b45cd7575dec
verbal-noun <thats.kaif@gmail.com>
2020-03-03 22:29:45 +1100

    Convolution and pooling

--- model.py
+++ model.py
@@ -30,7 +30,17 @@ tf_labels = tf.placeholder(tf.float32, shape=(None, LABELS))
 #generates a weight variable of a given shape.
 def weight_variable(shape):
   initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)#generates a bias variable of a given shape.
+  #generates a bias variable of a given shape.
+  return tf.Variable(initial)
 def bias_variable(shape):
   initial = tf.constant(0.1, shape=shape)
   return tf.Variable(initial)
+
+
+#returns a 2d convolution layer with full stride
+def conv_2d(x, W):
+    #down samples a feature map by 2X
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                          strides=[1, 2, 2, 1], padding='SAME')
